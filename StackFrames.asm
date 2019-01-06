50c43aa68513604292a3a4df184c684531bebd90
SleepingFox <seanloganking@gmail.com>
2019-01-06 17:01:39 -0400

    Functions can now return variables

--- StackFrames.asm
+++ StackFrames.asm
@@ -12,16 +12,23 @@ func:
 	; declare function variables
 	PUSH 1
 	PUSH 2
+	
+	; Declare local variable, equal to argument
+	MOV A, B
+	ADD A, 3
+	MOV A, [A]
+	PUSH A
 
 	;call another function
 	PUSH 5
 	CALL func2
-	
-	; Put argument in stack frame
-	MOV A, B	
+
+	POP D	; Puts what last function returned in D
+
+	;return a variable
+	MOV A, B
 	ADD A, 3
-	MOV A, [A]
-	PUSH A
+	MOV [A], D	;returns D
 
 	; leave
 	MOV SP, B 
@@ -38,11 +45,18 @@ func2:
 	PUSH 1
 	PUSH 2
 
-	; Put argument in stack frame
+	; Declare local variable, equal to argument
 	MOV A, B	
 	ADD A, 3
 	MOV A, [A]
 	PUSH A
+
+	MOV D, 6	; We will be returning what is stored in D
+
+	;return a variable
+	MOV A, B
+	ADD A, 3
+	MOV [A], D	;returns D
 
 	; leave
 	MOV SP, B 
@@ -53,5 +67,7 @@ func2:
 main:
 	PUSH 10
 	CALL func
+
+	POP A	;put what was returned in a register
 
 	HLT
