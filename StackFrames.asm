5be6e2d2647860314462bed56a70ed14040f922e
SleepingFox <seanloganking@gmail.com>
2019-01-06 16:21:37 -0400

    Functions called by functions
    A method for functions to return a result has not been finished yet.

--- StackFrames.asm
+++ StackFrames.asm
@@ -12,7 +12,32 @@ func:
 	; declare function variables
 	PUSH 1
 	PUSH 2
+
+	;call another function
+	PUSH 5
+	CALL func2
 	
+	; Put argument in stack frame
+	MOV A, B	
+	ADD A, 3
+	MOV A, [A]
+	PUSH A
+
+	; leave
+	MOV SP, B 
+	POP B
+
+	RET
+
+func2:
+	; Push BP and move SP into BP
+	PUSH B
+	MOV B, SP
+
+	; declare function variables
+	PUSH 1
+	PUSH 2
+
 	; Put argument in stack frame
 	MOV A, B	
 	ADD A, 3
